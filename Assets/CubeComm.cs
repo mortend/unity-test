c53c70fc71aa3f41d37057f3e7f30d30d15651ca
Morten Daniel Fornes <morten@fusetools.com>
2022-03-28 16:44:43 +0700

    [wip] compile RNBridge.cs

--- Assets/CubeComm.cs
+++ Assets/CubeComm.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Azesmway.RNU;
 using UnityEngine;
 
-public class CubeComm : MonoBehaviour
+public class CubeComm : MonoBehaviour, IRNCommandsReceiver
 {
     void Awake()
     {
-        
+        RNBridge.RegisterCommandsReceiver(this);
+    }
+
+    public void HandleCommand(RNCommand command)
+    {
+        switch (command.name)
+        {
+            default:
+                // command.Resolve(new {}) || command.Reject(new {})
+                break;
+        }
     }
 }
