211ab17aa3343128ad18774bd17747a34941d504
Morten Daniel Fornes <morten@fusetools.com>
2022-04-05 14:14:22 +0700

    update example

--- Assets/Example/SpinCube.cs
+++ Assets/Example/SpinCube.cs
@@ -1,25 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+
 using UnityEngine;
+using RNUnity;
 
 public class SpinCube : MonoBehaviour
 {
+    bool _rotate;
+    bool _scale;
+
     public Vector3 RotateAmount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rotate = true;
+        _scale = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(RotateAmount);
+        if (_rotate)
+            transform.Rotate(RotateAmount);
     }
 
-    void resetRotation()
+    void toggleRotate()
     {
-        RotateAmount = default(Vector3);
+        _rotate = !_rotate;
     }
+
+    void toggleScale()
+    {
+        _scale = !_scale;
+        transform.localScale *= _scale ? 2.0f : 0.5f;
+    }
+
+    void OnMouseDown()
+    {
+        RNBridge.SendMessage("click!");
+        toggleScale();
+    }
+
+    // Call the following methods from your RN app
+
+    void toggleRotateRN(object param)
+    {
+        toggleRotate();
+        RNBridge
+            .Begin(param)
+            .Resolve(_rotate);
+    }
+
+    void getAccountRN(object param)
+    {
+        RNBridge
+            .Begin(param)
+            .Resolve(new Account
+            {
+                Email = "james@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string>
+                {
+                    "User",
+                    "Admin"
+                }
+            });
+    }
+
+    void failRN(object param)
+    {
+        RNBridge
+            .Begin(param)
+            .Reject("This doesn't work");
+    }
+}
+
+public class Account
+{
+    public string Email { get; set; }
+    public bool Active { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public IList<string> Roles { get; set; }
 }
