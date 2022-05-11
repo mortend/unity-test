1089ef4096fc7987b89217f1aa4129a437ef0995
Morten Daniel Fornes <morten@fusetools.com>
2022-05-11 14:51:02 +0700

    Revert "refactor RNUProxy.m (fix linker error on device)"
    This reverts commit 1f80aace0bad0b9af26c6f057a832e3349c2014f.

--- Assets/RNUnity/RNUProxy.m
+++ Assets/RNUnity/RNUProxy.m
@@ -1,11 +1,11 @@
 #import "RNUProxy.h"
 
-/** Implemented by react-native-unity2 to receive messages from Unity */
-void (*RNUProxyEmitEvent_fun)(const char*, const char*) = NULL;
+@interface RNUnity : NSObject
+
++ (void)emitEvent:(const char*)name data:(const char*)data;
+
+@end
 
 void RNUProxyEmitEvent(const char* name, const char* data) {
-    if (RNUProxyEmitEvent_fun)
-        (*RNUProxyEmitEvent_fun)(name, data);
-    else
-        NSLog(@"ERROR: RNUProxyEmitEvent_fun was not assigned!");
+    [RNUnity emitEvent:name data:data];
 }
